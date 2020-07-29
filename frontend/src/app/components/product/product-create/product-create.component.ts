c9a8fdb39e0de5dd66d56324861e4ef22f359312
Rafaela <rafaelaperes215@gmail.com>
2020-07-29 18:54:06 -0300

    Atualizado

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
@@ -9,7 +10,10 @@ import {Router} from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
-  
+  product: Product = {
+    name: '',
+    price: null
+  }
 
   constructor(private productService: ProductService, private router: Router) { }
 
@@ -18,7 +22,12 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage("Produto salvo com sucesso :)")
+
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto salvo com sucesso :)")
+      this.router.navigate(['/products'])
+    })
+    
   }
 
   cancel(): void {
