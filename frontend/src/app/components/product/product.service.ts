d8c00c4f46dec40cd221dd968518cc852ef0b51d
Rafaela <rafaelaperes215@gmail.com>
2020-08-03 18:25:51 -0300

    Adicionado os metodos create, read, readById e update para manipular os dados no backend

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -21,12 +21,26 @@ export class ProductService {
     })
   }
 
+  //Insere dados do produto no backend // Insert product data in the backend
  create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product)
  }
 
+ //Lê os dados do backend // Reads backend data
  read(): Observable<Product[]> {
    return this.http.get<Product[]>(this.baseUrl)
+ }
+
+ //Lê os dados por meio do id do produto // Reads data via product id
+ readById(id: String): Observable<Product> {
+  const url = `${this.baseUrl}/${id}`
+  return this.http.get<Product>(url)
+ }
+
+//Atualizar os dados do produto // Update product data
+ update(product: Product): Observable<Product> {
+  const url = `${this.baseUrl}/${product.id}`
+  return this.http.put<Product>(url, product)
  }
 
 }
